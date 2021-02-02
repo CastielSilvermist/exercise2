931631119f02b31251e2189d3c19049c46987add
Jorinda Becker <jorindabecker@gmail.com>
2021-02-01 17:34:57 -0700

    working with jQuery. Stuck on event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,51 @@
 console.log("hello");
 
+var $one = $("#j1");
+var $two = $("#a1");
+
+var $three = $("#j2");
+var $four = $("#a2");
+
+var $five = $("#j3");
+var $six = $("#a3");
+
+
+
+// There must be an easier way. Replace text rather than que
+// variables. Maybe an array? Unsure how to replace text on click.
+
+
+$(".joke").on("click", function() {
+	$one.hide();
+	$two.show();
+
+	$(".joke").on("click", function() {
+
+		$two.hide();
+		$three.show();
+
+			$(".joke").on("click", function() {
+
+				$three.hide();
+				$four.show();
+					$(".joke").on("click", function() {
+
+						$four.hide();
+						$five.show();
+							
+							$(".joke").on("click", function() {
+
+							$five.hide();
+							$six.show();
+										
+							});	
+							
+					});
+
+			});
+	});
+	
+});
+
+
+
